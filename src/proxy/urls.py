b7fc126b4f333134b2a3b6e64ed359dbb2b7fb6f
bradyt <bradyt@users.noreply.github.com>
2017-07-30 09:29:23 -0700

    Removes `patterns` missing since Django 1.10
    `patterns` was deprecated in Django 1.8
    
    From https://docs.djangoproject.com/en/1.11/releases/1.10/, "django.conf.urls.patterns() is removed."

--- src/proxy/urls.py
+++ src/proxy/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 from proxyapp.views import TestProxyView
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'proxy.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^(?P<path>.*)$', TestProxyView.as_view()),
-)
+]
